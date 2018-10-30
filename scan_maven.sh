b071a98bd52a424d01fabb8c95970c13c9079b86
Geiselmann, Tobias <Tobias.Geiselmann@senacor.com>
2018-10-30 17:30:43 +0100

    Updated scripts to use project path as parameter

--- scan_maven.sh
+++ scan_maven.sh
@@ -1,19 +1,34 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+  then
+    echo "No project path supplied, using default path /scan"
+    path="/scan"
+  else
+    path=$1
+fi
+
+# Create report directory if it doesn't already exist
+if [ ! -d "$path/report" ]; then
+    mkdir $path/report
+fi
+
 # Run dependency-check
 echo "Running dependency-check..."
-#dependency-check/bin/dependency-check.sh --project myproject --out /scan/report/ --scan scan/
+/dependency-check/bin/dependency-check.sh --project myproject --out $1/report/ --scan $1
 echo "Finished running dependency-check. Report can be found under /scan/report."
 
 # Run license_finder
 echo "Running license_finder..."
-license_finder report --save=/scan/report/license_report --project-path=/scan
+license_finder report --save=$1/report/license_report --project-path=$1
 echo "Finished running license_finder."
 
 # Run snyk
 echo "Running snyk..."
-(cd scan && snyk test >> /scan/report/snyk 2>&1)
+(cd $1 && snyk test >> report/snyk 2>&1)
 echo "Finished running snyk."
 
 # Run OSSIndex
 echo "Running OSSIndex..."
-mvn org.sonatype.ossindex.maven:ossindex-maven-plugin:audit -Dossindex.reportFile=/report/ossindex_report.txt -f scan/pom.xml
+mvn org.sonatype.ossindex.maven:ossindex-maven-plugin:audit -Dossindex.reportFile=/report/ossindex_report.txt -f $1/pom.xml
 echo "Finished running OSSIndex."
--- scan_node.sh
+++ scan_node.sh
@@ -1,24 +1,39 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+  then
+    echo "No project path supplied, using default path /scan"
+    path="/scan"
+  else
+    path=$1
+fi
+
+# Create report directory if it doesn't already exist
+if [ ! -d "$path/report" ]; then
+    mkdir $path/report
+fi
+
 # Run dependency-check
 echo "Running dependency-check..."
-dependency-check/bin/dependency-check.sh --project myproject --out /scan/report/ --scan scan/
+/dependency-check/bin/dependency-check.sh --project myproject --out $1/report/ --scan $1
 echo "Finished running dependency-check. Report can be found under /scan/report."
 
 # Run license_finder
 echo "Running license_finder..."
-license_finder report --save=/scan/report/license_report --project-path=/scan
+license_finder report --save=$1/report/license_report --project-path=$1
 echo "Finished running license_finder."
 
 # Run snyk
 echo "Running snyk..."
-(cd scan && snyk test >> /scan/report/snyk 2>&1)
+(cd $1 && snyk test >> report/snyk 2>&1)
 echo "Finished running snyk."
 
 # Run npm audit
 echo "Running npm audit..."
-(cd scan && npm audit >> /scan/report/npm_audit 2>&1)
+(cd $1 && npm audit >> report/npm_audit 2>&1)
 echo "Finished running npm audit."
 
 # Run auditjs
 echo "Running auditjs..."
-(cd scan && auditjs >> /scan/report/auditjs 2>&1)
+(cd $1 && auditjs >> report/auditjs 2>&1)
 echo "Finished running auditjs."
